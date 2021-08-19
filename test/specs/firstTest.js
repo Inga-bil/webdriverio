32c88ac88b0f66c64ec83451f1d8392dc1780015
Inga Bilyk <inga11bil@gmail.com>
2021-08-19 17:59:28 -0600

    new changes

--- test/specs/firstTest.js
+++ test/specs/firstTest.js
@@ -5,11 +5,26 @@ describe('Ecommerce application', ()=>{
         //webdriverIO code
         browser.url("https://the-internet.herokuapp.com/login")
         console.log(browser.getTitle())
-        browser.pause(3000)
+       
 
         //check is the title is the one that we need (the correct one)
        expect(browser).toHaveTitleContaining("The Internet")
 
+       const username = $("#username");
+       username.setValue("Hello World");
+       
+
+       const password = $("//*[@name = 'password']");
+       password.setValue("1212hgjhvhgv");
+
+       const login = $("//*[@type = 'submit']");
+       login.click();
+
+       const errorMessage = $(".flash");
+       console.log (errorMessage.getText() + "is the message that is shown on the screen");
+
+       browser.pause(3000)
+       
 
     })
 
