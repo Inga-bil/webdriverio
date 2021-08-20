fcae3fa624fc1c07065331939d9856d6ebbc3608
Inga Bilyk <inga11bil@gmail.com>
2021-08-19 23:26:06 -0600

    assertion

--- test/specs/firstTest.js
+++ test/specs/firstTest.js
@@ -23,6 +23,11 @@ describe('Ecommerce application', ()=>{
        const errorMessage = $(".flash");
        console.log (errorMessage.getText() + "is the message that is shown on the screen");
 
+       //assersion on the text that is prsent on the screen to verify if it is the right text
+
+       $("h4.subheader").getText();
+       expect($("h4.subheader")).toHaveTextContaining("This is where you can log into the secure area. Enter tomsmith for the username and SuperSecretPassword! for the password. If the information is wrong you should see error messages.")
+
        browser.pause(3000)
        
 
