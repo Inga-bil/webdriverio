c6cc6c4349381b0fec258134ca2d08f6f0f01316
Inga Bilyk <inga11bil@gmail.com>
2021-08-30 14:03:04 -0700

    windowsFrames

--- test/specs/windowsFrames.js
+++ test/specs/windowsFrames.js
@@ -1,13 +1,48 @@
 describe('Windows and Frames Miscellanous', ()=>{
 
-    it('Parent and Child window switch', ()=> {
+    xit('Parent and Child window switch', ()=> {
 
-        $("[onclick*='openWindow']").click()
-        let handles = browser.getWindowHandle()
+        browser.url("https://www.rahulshettyacademy.com/AutomationPractice/")
+
+        $("#openwindow").click()
+        let handles = browser.getWindowHandles()
         //0 index first window, 1 index child window
         browser.switchToWindow(handles[1])
         console.log(browser.getTitle())
 
+        browser.switchToWindow(handles[0])
+        console.log(browser.getTitle())
+
+
+    })
+
+    xit('Switch windows and grab text', ()=>{
+
+
+        browser.url("https://rahulshettyacademy.com/#/index")
+        const title = browser.getTitle()
+        console.log(title)
+
+        browser.newWindow("https://www.rahulshettyacademy.com/AutomationPractice/")
+        $("#name").setValue(title)
+
+        browser.switchWindow(title)
+
+        browser.pause(3000)
+
+    })
+
+    it('Frames switch',()=>{
+
+        browser.url("https://www.rahulshettyacademy.com/AutomationPractice/")
+
+        $("#mousehover").scrollIntoView()
+        console.log($$("a").length)
+        browser.switchToFrame($("[id ='courses-iframe']"))
+        console.log($("=Courses").getTagName())
+        console.log($$("a").length)
+        browser.switchToFrame(null)
+        console.log($$("a").length)
 
     })
 })
