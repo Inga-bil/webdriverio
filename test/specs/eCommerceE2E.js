59adebda20c691cac0f9d345d738323273916c43
Inga Bilyk <inga11bil@gmail.com>
2021-08-31 13:17:40 -0700

    sumOfProducts

--- test/specs/eCommerceE2E.js
+++ test/specs/eCommerceE2E.js
@@ -2,16 +2,38 @@ describe('Ecommerce Application', ()=>{
 
     it('End to End test', ()=>{
 
+        var products = ["Blackberry", "Nokia Edge"]
+
         browser.url("https://www.rahulshettyacademy.com/loginpagePractise/")
 
         const username = $("#username")
         username.setValue("rahulshettyacademy")
-        const password = $(".form-control")
+        const password = $("#password")
         password.setValue("learning")
 
         $("#signInBtn").click()
 
-        browser.pause(2000)
+        const link = $("*=Checkout")
+        link.waitForExist()
+        console.log(browser.getTitle())
+
+        let cards = $$("div[class='card h-100']")
+        cards.filter(card => products.includes(card.$("div h4 a").getText()))
+        .map(productCard => productCard.$(".card-footer button").click())
+        link.click()
+
+        productPrices = $$("//tr/td[4]/strong")
+        //getText
+        //format it and convert into integer and apply sum on all 
+        const sumOfProducts = productPrices.map(productPrice =>parseInt(productPrice.getText().split(".")[1].trim()))
+        .reduce((acc,price)=> acc+price, 0)
+        console.log(sumOfProducts)
+
+        browser.pause(3000)
+        
+
+
+        
 
     })
 })
